a1df8aefe248dcc8f52c59abb4fa677a5f4ae4b7
bram <david.j.bramwell@googlemail.com>
2018-10-21 22:07:01 +0100

    Some site functionality

--- site/client/src/App.js
+++ site/client/src/App.js
@@ -3,22 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    year: new Date().getFullYear(),
+    month: new Date().toLocaleString("en-us", {month: "long"})
+  }
+
+  fetchWaterings() {
+    var endpoint = 'https://00ix9zye5i.execute-api.us-east-1.amazonaws.com/dev/waterings?'
+    if (this.state.month) {
+      endpoint += `year=${this.state.year}`
+    }
+    if (this.state.month) {
+      endpoint += `&month=${this.state.month}`
+    }
+    fetch(endpoint, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      this.setState({data: json})
+    });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{
+          backgroundImage: `url(${require("./images/background.jpg")})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }} >
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Laura's Bonsai</h1>
+          <a href="#" onClick={(event) => {
+            event.preventDefault()
+            this.fetchWaterings()
+          }}>Fetch Waterings</a>
+          <p>{JSON.stringify(this.state.data)}</p>
         </header>
       </div>
     );
